fd02c82c02ed9d4ee31af7af5c51d99dbcfe229b
Kseniia <k.feskova@itransition.com>
2022-12-05 13:08:57 +0200

    changed the secret

--- app/src/main/java/com/example/education/MainActivity.kt
+++ app/src/main/java/com/example/education/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AppCenter.start(
-            application, key,
+            application, hey,
             Analytics::class.java, Crashes::class.java
         )
         binding = ActivityMainBinding.inflate(layoutInflater)
