7ed023d776be2dfa5de486e8e88ee6d1e85e0369
Kseniia <k.feskova@itransition.com>
2022-12-05 19:12:23 +0200

    changed yaml for firebase

--- app/src/main/java/com/example/education/MainActivity.kt
+++ app/src/main/java/com/example/education/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.education
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.education.databinding.ActivityMainBinding
+import com.google.firebase.messaging.FirebaseMessaging
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,5 +22,19 @@ class MainActivity : AppCompatActivity() {
             setReorderingAllowed(true)
             add<ListFragment>(R.id.fragment)
         }
+        FirebaseMessaging.getInstance().token.addOnSuccessListener { token ->
+            if (!TextUtils.isEmpty(token)) {
+                Log.d(TAG, "retrieve token successful : $token")
+            } else {
+                Log.w(TAG, "token should not be null...")
+            }
+        }.addOnFailureListener { e -> }
+            .addOnCanceledListener {}.addOnCompleteListener { task ->
+                Log.v(TAG, "This is the token : " + task.result)
+            }
+
+    }
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
